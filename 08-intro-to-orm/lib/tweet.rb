dec4d42ca2b26e134774d033ed621dc775ae127e
kingcons <brit@kingcons.io>
2018-12-19 10:46:31 -0500

    Built out a rudimentary ORM system.

--- 08-intro-to-orm/lib/tweet.rb
+++ 08-intro-to-orm/lib/tweet.rb
@@ -1,14 +1,26 @@
 class Tweet
-  attr_accessor :message, :username
-  ALL = []
+  attr_accessor :id, :message, :username
 
   def self.all
-    ALL
+    hashes = DB[:conn].execute("SELECT * FROM tweets")
+    hashes.map do |hash|
+      Tweet.new(hash)
+    end
   end
 
   def initialize(props={})
+    @id = props['id']
     @message = props['message']
     @username = props['username']
-    ALL << self
+    self.save
+  end
+
+  def save
+    return if self.id
+    sql_code = <<-example
+      INSERT INTO tweets (username, message)
+        VALUES ('#{@username}', '#{@message}')
+    example
+    DB[:conn].execute(sql_code)
   end
 end
