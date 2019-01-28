a8a3ce580aa87688144fcfae40c4c1828337f255
Safae Alaoui <salaoui@NYC-US08911-MBP.local>
2019-01-28 14:50:09 -0500

    added scopes and callback lecture

---
+++ 19-scope-and-callback/index.js
@@ -0,0 +1,20 @@
+
+console.log(x)
+
+printName()
+
+var x = 2 ;
+
+function test(){
+
+	testingGlobalVar = 12 
+}
+
+console.log(testingGlobalVar)
+
+function printName(){
+
+	console.log("Norma")
+
+}
+
