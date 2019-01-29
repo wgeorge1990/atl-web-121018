55b94d84469f6969e18dbad11d6f33cc8abe13a3
Safae Alaoui <salaoui@NYC-US08911-MBP.attlocal.net>
2019-01-29 11:05:22 -0500

    Adding Dom intro lecture

---
+++ 20-the-dom/src/index.js
@@ -0,0 +1,16 @@
+console.log('%c Hello, World!', 'color: orange')
+
+// DO NOT DO THIS:
+
+
+const container = document.querySelector("")
+
+dankMemes.forEach(function(imageURL){
+
+	const imgElement = document.createElement("img")
+
+	imgElement.src = imageURL
+
+	container.appendChild(imgElement)
+
+})
