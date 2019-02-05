1d749449cb36858fedcb47e75b0e2fbdd400006c
kingcons <brit@kingcons.io>
2019-02-05 12:26:55 -0500

    Add finished code from this lecture.

--- 25-this/index.js
+++ 25-this/index.js
@@ -1,5 +1,67 @@
 this.name = 'window object'
 
+// let dog = {
+//   name: 'seyla',
+//   needsHaircut: true,
+//   groom: function () {
+//     console.log(this);
+//     if (this.needsHaircut) {
+//       console.log("seyla gets her hair did. how cute!");
+//       this.needsHaircut = false;
+//     } else {
+//       console.log("nah, gonna roll around outside")
+//     }
+//   }
+// }
+
+function Dog(name) {
+  console.log(this)
+  this.name = name
+  this.needsHaircut = true
+  this.groom = () => {
+    console.log(this)
+    if (this.needsHaircut) {
+      console.log("seyla gets her hair did. how cute!");
+    } else {
+      console.log("nah, gonna roll around outside")
+    }
+  }
+  // this.groom = function () {
+  //   console.log(this);
+  //   if (this.needsHaircut) {
+  //     console.log("seyla gets her hair did. how cute!");
+  //     this.needsHaircut = false;
+  //   } else {
+  //     console.log("nah, gonna roll around outside")
+  //   }
+  // }.bind(this)
+  // this.groom = this.groom.bind(this)
+}
+
+let example = Dog('seyla')
+let seyla = new Dog('seyla')
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  let button = document.querySelector("button")
+  // would like to say button.addEventListener('click', dog.groom)
+  // button.addEventListener('click', dog.groom.bind(dog))
+  button.addEventListener('click', seyla.groom)
+  // obj.method.bind(obj)
+})
+
+/* Know the defaults */
+
+function nonStrictTest() {
+  console.log(this); // should log out Window
+}
+
+function strictTest() {
+  'use strict';
+  console.log(this); // logs out undefined
+}
+
+
 /************ Function Called with New Keyword ***********/
 //inside a constructor fn, this will be the newly created object
 
--- 25-this/self.rb
+++ 25-this/self.rb
@@ -1,5 +1,6 @@
 class Person
-  attr_accessor :name
+  self.attr_accessor(:name)
+
   def initialize(name)
     @name = name
   end
@@ -7,11 +8,15 @@ class Person
   def self.say_something
     puts "I am a class method defined on the #{self} class."
   end
+
+  def greet
+    puts "I'm #{self.name}. Nice to meet you."
+  end
 end
 
 Person.say_something # => self will be Person class
 # self in ruby is the receiver of a method
 
 blair = Person.new('blair')
-blair.name # => self will be the instance
+blair.greet # => self will be the instance
 # blair (instance) is receiving the method call
